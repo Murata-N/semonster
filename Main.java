c3ce8026f4933a373d8239faa4ace36f6cd435f1
Murata-N <m1m22a29@st.oit.ac.jp>
2022-06-13 17:46:01 +0900

    add semonster main class

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello SEMonster");
+    System.out.println("SEMONSTER!!");
+    Monster monster = new Monster();
+    System.out.print(monster.toString());
   }
 }
