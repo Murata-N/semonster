a1882e1431d5c8416e0d286294b0afd4b0607890
Murata-N <m1m22a29@st.oit.ac.jp>
2022-07-14 13:05:37 +0900

    update Main.java

--- .log/.history/semonster/Main_20220714130334.java
+++ .log/.history/semonster/Main_20220714130334.java
@@ -1,8 +1,5 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("SEMONSTER!!");
-    Player player = new Player();
-    player.drawMonsters();
-    player.showDeck();
   }
 }
